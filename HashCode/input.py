bc753192bcd28c53261502b890ec16dba161633a
howardhall <howardhall118@gmail.com>
2020-02-20 18:46:47 +0000

    library class includes library number

--- HashCode/input.py
+++ HashCode/input.py
@@ -1,6 +1,8 @@
+#!/usr/bin/python3
 class Lib:
-    def __init__(self, s):
+    def __init__(self, s, numx):
         t = s.split()
+        self.lib_num = numx
         self.book_count = int(t[0])
         self.signup_days = int(t[1])
         self.ship_per_day = int(t[2])
@@ -10,6 +12,7 @@ class Lib:
         for i in t:
             self.books.append(int(i))
     def display(self):
+        print("\n\t\t Library Number:", self.lib_num)
         print("book count:", self.book_count)
         print("Signup days:", self.signup_days)
         print("Ship per day:", self.ship_per_day)
@@ -29,7 +32,7 @@ def read(fname):
     data["Libs"] = []
     for i in range(data["LibCount"]):
         line = fp.readline()
-        newLib = Lib(line)
+        newLib = Lib(line,i)
         line = fp.readline()
         newLib.load_books(line)
         data["Libs"].append(newLib)
